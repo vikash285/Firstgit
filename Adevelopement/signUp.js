5fa7c44b17ce7967ff896aa017f1f94dc7f1f3d7
vikash285 <vikashdhaka285@gmail.com>
2023-02-17 12:02:38 +0530

    forntend signUp page

---
+++ Adevelopement/signUp.js
@@ -0,0 +1,21 @@
+async function signUp(event) {
+    try {
+        event.preventDefault()
+
+       const signUpDeatils = {
+        name: event.target.name.value,
+        email: event.target.email.value,
+        password: event.target.password.value
+       }
+       console.log(signUpDeatils)
+
+       const res = await axios.post("http://localhost:3000/user/signUp", signUpDeatils)
+       if (res.status === 201 ) {
+           window.location.href = "../Login/login.html"
+       } else {
+           throw new Error('Failed to Login')
+       }
+    } catch (err) {
+       document.body.innerHTML += `<div style="color:red;">${err}</div>`
+    }
+}
