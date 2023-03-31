f70dc9301ac6d8ad3a761909627801447dd4a0e7
vikash285 <vikashdhaka285@gmail.com>
2023-03-31 15:32:37 +0530

    creating ts apis put,post,delete,get

---
+++ routes/todos.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express'
+
+import { Todo } from '../models/todos'
+
+let todos: Todo[] = []
+
+const router = Router()
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({ todos: todos })
+})
+
+router.post('/todo', (req, res, next) => {
+    const newTodo: Todo = {
+        id: new Date().toISOString(),
+        text: req.body.text
+    }
+    todos.push(newTodo)
+    res.status(201).json({ message: 'Added todo', todo: newTodo, todos: todos })
+})
+
+router.put('/todo/:todoId', (req, res, next) => {
+  const tid = req.params.todoId
+  const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid)
+  if (todoIndex >= 0) {
+    todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text }
+    return res.status(200).json({ message: 'Updated todo', todos: todos })
+  }
+  res.status(404).json({ message: 'Could not find'})
+})
+
+router.delete('/todo/:todoId', (req, res, next) => {
+    todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId)
+    res.status(200).json({ message: 'Deleted todo', todos: todos })
+})
+
+export default router
